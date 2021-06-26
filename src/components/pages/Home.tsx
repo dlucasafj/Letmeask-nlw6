32dce99c6051f10722770c805bdb6d50bb97a876
danielLucas <daniel.lucasafj@gmail.com>
2021-06-26 15:41:34 -0300

    Autenticação com Firebase

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,14 +4,24 @@ import logo from  '../../assets/images/logo.svg';
 import googleIconImg from '../../assets/images/google-icon.svg';
 import '../../styles/auth.scss'
 import { Button } from '../Button';
+import {auth, firebase} from '../../services/firebase'
 
 
 export function Home(){
 
     const history = useHistory();
+
     function navigateToNewRoom(){
-        history.push('/rooms/new')
+        
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result=>{
+            console.log(result);
+        })
+
+        // history.push('/rooms/new')
     }
+
     return(
         <div id='page-auth'>
             <aside>
